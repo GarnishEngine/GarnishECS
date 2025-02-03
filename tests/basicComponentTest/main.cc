f7c88f9facf9df92cdcb8497544a9d702ec61d86
Benjamin Bingham <ben@benbingham.ca>
2025-02-02 17:03:49 -0700

    feat: Added checking for component existance

--- tests/basicComponentTest/main.cc
+++ tests/basicComponentTest/main.cc
@@ -4,14 +4,25 @@
 
 #include "garnish_ecs.h"
 
-struct A { };
+struct A { 
+    A() {
+        // std::cout << "Constructor" << std::endl;
+    }
+
+    ~A() {
+        // std::cout << "Destructor" << std::endl;
+    }
+};
 
 int main (void) {
     garnish::ECSManager ecs;
     ecs.RegisterComponent<A>();
 
-    auto ent = ecs.CreateEntity();
-    ecs.AddComponent<A>(ent, A{ });
+    auto e = ecs.CreateEntity();
 
-    // assert(ecs.HasComponent<A>(ent) && "Entity dosent have component just added to it"); // TODO
+    assert(!ecs.HasComponent<A>(e) && "Entity has component that has not been added to it");
+
+    ecs.AddComponent<A>(e, A{ });
+
+    assert(ecs.HasComponent<A>(e) && "Entity does not have component just added to it");
 }
